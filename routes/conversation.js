71f470d7d5c0d0020b0ccdb5b2e40bd2347bbda6
fareed <fareed@qetaa.com>
2021-08-18 01:57:46 +0300

    added conversation summary endpoint

--- routes/conversation.js
+++ routes/conversation.js
@@ -1,34 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const Conversation = require('../model/Conversation');
+const Conversation = require("../model/Conversation");
 
-
-router.post('/', async (req, res) => {
-    try {
-        const newConversation = new Conversation({
-            members: req.body.members
-        })
-        const savedConversation = await newConversation.save();
-        res.status(200).json(savedConversation);
-
-    } catch (error) {
-        console.log("error", error);
-        res.status(500).json(error);
-    }
+router.post("/", async (req, res) => {
+  try {
+    const newConversation = new Conversation({
+      members: req.body.members,
+    });
+    const savedConversation = await newConversation.save();
+    res.status(200).json(savedConversation);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:userId', async (req, res) => {
-    try {
-        const conversation = await Conversation.find({
-            "members.id": req.params.userId
-        })
-        res.status(200).json(conversation);
-
-    } catch (error) {
-        console.log("error", error);
-        res.status(500).json(error);
-    }
+router.get("/conversations/summary", async (req, res) => {
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const totalConversations = await Conversation.count(); //total documents
+  const todayConversations = await Conversation.find({
+    createdAt: { $gte: today },
+  }).count();
+  const result = { totalConversations, todayConversations };
+  return res.status(200).json(result);
 });
 
+router.get("/:userId", async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      "members.id": req.params.userId,
+    });
+    res.status(200).json(conversation);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,54 +2,50 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const evn = require("dotenv");
-const mongoose = require('mongoose');
-const fs = require('fs');
+const mongoose = require("mongoose");
+const fs = require("fs");
 
-const conversationRoutes = require('./routes/conversation');
-const messageRoutes = require('./routes/messages');
-
+const conversationRoutes = require("./routes/conversation");
+const messageRoutes = require("./routes/messages");
 
 evn.config();
 
 app.use(express.json());
 app.use(cors());
 
-
-const isProd = process.env.MONGODB_HOST !== 'localhost';
+const isProd = process.env.MONGODB_HOST !== "localhost";
 console.log(isProd);
-const db_connection_Url = isProd ?
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_CHAT_DB}`
-    :`mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_CHAT_DB}`;
+const db_connection_Url = isProd
+  ? `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_CHAT_DB}`
+  : `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_CHAT_DB}`;
 
 console.log(db_connection_Url);
-const options = isProd ? { useNewUrlParser: true,
-    auth: {
-        authSource : "admin"
-    },
-    ssl:true,
-    sslValidate: false, 
-    sslCA: fs.readFileSync(`${process.env.MONGODB_CERT_FILE}`)
-    
-} : {}; 
+const options = isProd
+  ? {
+      useNewUrlParser: true,
+      auth: {
+        authSource: "admin",
+      },
+      ssl: true,
+      sslValidate: false,
+      sslCA: fs.readFileSync(`${process.env.MONGODB_CERT_FILE}`),
+    }
+  : {};
 
+mongoose
+  .connect(db_connection_Url, options)
+  .then(() => console.log("Connected to MongoDB...."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
 
-mongoose.connect(db_connection_Url, options)
-    .then(() => console.log('Connected to MongoDB....'))
-    .catch(err => console.log('Could not connect to MongoDB...', err));
-
-app.use('/conversation', conversationRoutes);
-app.use('/messages', messageRoutes);
-
-
-
-
+app.use("/conversation", conversationRoutes);
+app.use("/messages", messageRoutes);
 
 app.get("/", (req, res) => {
-    res.send("hello world!");
+  res.send("hello world!");
 });
 
 const PORT = process.env.APP_PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`app started on port ${PORT}`)
+  console.log(`app started on port ${PORT}`);
 });
