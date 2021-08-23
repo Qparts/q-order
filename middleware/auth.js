c790cf65c343f90f6c2e596c771450e61b565f52
fareed <fareed@qetaa.com>
2021-08-23 14:43:17 +0300

    updated env names

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,34 @@
-
-const _ = require('lodash')
-const fs = require('fs')
-const jwt = require('jsonwebtoken')
+const _ = require("lodash");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-
-  let publicKey = fs.readFileSync(`${process.env.FILE_DIRECTORY}/${process.env.PUBLIC_KEY_FILE}`, 'utf8');
+  let publicKey = fs.readFileSync(
+    `${process.env.KEY_FILE_DIRECTORY}/${process.env.KEY_FILE}`,
+    "utf8"
+  );
 
   // publicKey = publicKey.replace(/\n/g, "").replace(PUBLIC_KEY_HEADER, "").replace(PUBLIC_KEY_FOOTER, "");
 
-  let token = req.header('AUTHORIZATION');
+  let token = req.header("AUTHORIZATION");
 
-  if (!token)
-    return res.status(401).send("Access denied, No token provided.");
+  if (!token) return res.status(401).send("Access denied, No token provided.");
 
   token = token.substring("Bearer".length).trim();
 
-  jwt.verify(token, publicKey, { algorithms: ['RS256'] }, function (err, decoded) {
-    if (err) {
-      return res.status(401).send("User cannot access the resource.");
-    } else {
-      const loginUser = _.pick(decoded, ['comp', 'typ', 'appCode', 'sub'])
-      console.log("loginUser", loginUser);
-      req.user = loginUser;
-      next();
+  jwt.verify(
+    token,
+    publicKey,
+    { algorithms: ["RS256"] },
+    function (err, decoded) {
+      if (err) {
+        return res.status(401).send("User cannot access the resource.");
+      } else {
+        const loginUser = _.pick(decoded, ["comp", "typ", "appCode", "sub"]);
+        console.log("loginUser", loginUser);
+        req.user = loginUser;
+        next();
+      }
     }
-  });
-
-}
+  );
+};
