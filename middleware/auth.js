6f2f1e7e8ed0ebf9e4a1a184aeec2d45941ede28
fareed <fareed@qetaa.com>
2021-08-29 00:55:03 +0300

    added logs

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,27 +4,28 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 module.exports = function (req, res, next) {
+  console.log("reading public key file");
   let publicKey = fs.readFileSync(
     `${process.env.KEY_FILE_DIRECTORY}/${process.env.KEY_FILE}`,
     "utf8"
   );
-
-  // publicKey = publicKey.replace(/\n/g, "").replace(PUBLIC_KEY_HEADER, "").replace(PUBLIC_KEY_FOOTER, "");
-
+  console.log("public key file read");
   let token = req.header("AUTHORIZATION");
-
+  console.log("reading token");
   if (!token) return res.status(401).send("Access denied, No token provided.");
-
   token = token.substring("Bearer".length).trim();
-
+  console.log("token is ", token);
+  console.log("verifying");
   jwt.verify(
     token,
     publicKey,
     { algorithms: ["RS256"] },
     function (err, decoded) {
       if (err) {
+        console.log("there is an error", err);
         return res.status(401).send("User cannot access the resource.");
       } else {
+        console.log("no error, will read login user");
         const loginUser = _.pick(decoded, ["comp", "typ", "appCode", "sub"]);
         console.log("loginUser", loginUser);
         req.user = loginUser;
