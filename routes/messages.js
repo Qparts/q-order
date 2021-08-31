ddf3f32b3dca5abd037da2bd664162832e3ba4e5
fareed <fareed@qetaa.com>
2021-08-31 12:28:26 +0300

    added limit to conversation load

--- routes/messages.js
+++ routes/messages.js
@@ -1,85 +1,84 @@
 const express = require("express");
 const router = express.Router();
-const Message = require('../model/Message');
-const auth = require('../middleware/auth')
+const Message = require("../model/Message");
+const auth = require("../middleware/auth");
 
-
-router.post('/', auth, async (req, res) => {
-    try {
-        const newMessage = new Message(req.body);
-        const savedMessage = await newMessage.save();
-        res.status(200).json(savedMessage);
-
-    } catch (error) {
-        res.status(500).json(error);
-    }
+router.post("/", auth, async (req, res) => {
+  try {
+    const newMessage = new Message(req.body);
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
+router.get("/:conversationId", auth, async (req, res) => {
+  try {
+    const count = +req.query.count;
+    const page = +req.query.page;
 
-router.get('/:conversationId', auth, async (req, res) => {
-    try {
-        const count = +req.query.count;
-        const page = +req.query.page;
+    const messages = await Message.paginate(
+      { conversationId: req.params.conversationId },
+      {
+        page,
+        limit: count,
+        sort: {
+          createdAt: -1, //Sort by Date Added DESC
+        },
+      }
+    );
 
-        const messages = await Message.paginate(
-            { conversationId: req.params.conversationId },
-            {
-                page,
-                limit: 10,
-                sort: {
-                    createdAt: -1 //Sort by Date Added DESC
-                }
-            }
-        );
+    const docs = messages.docs.sort(
+      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+    );
 
-        const docs = messages.docs.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-
-        res.status(200).json(docs);
-
-    } catch (error) {
-        res.status(500).json(error);
-    }
+    res.status(200).json(docs);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-
-router.post('/unseen', auth, async (req, res) => {
-    try {
-        const conversations = req.body.conversations;
-        const sender = req.body.sender;
-        const messages = await Message.find({
-            status: 'I',
-            sender: { $ne: sender },
-            conversationId: { $in: conversations }
-        });
-        res.status(200).json(messages);
-
-    } catch (error) {
-        res.status(500).json(error);
-    }
+router.post("/unseen", auth, async (req, res) => {
+  try {
+    const conversations = req.body.conversations;
+    const sender = req.body.sender;
+    const messages = await Message.find({
+      status: "I",
+      sender: { $ne: sender },
+      conversationId: { $in: conversations },
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put('/', auth, async (req, res) => {
-    try {
-        const message = new Message(req.body);
-        const updatedMessage = await Message.update({ _id: message._id }, message);
-        res.status(200).json(updatedMessage);
-    } catch (error) {
-        console.log("error", error);
-        res.status(500).json(error);
-    }
+router.put("/", auth, async (req, res) => {
+  try {
+    const message = new Message(req.body);
+    const updatedMessage = await Message.update({ _id: message._id }, message);
+    res.status(200).json(updatedMessage);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json(error);
+  }
 });
 
-router.put('/seen/:conversationId/:sender', auth, async (req, res) => {
-    try {
-        const updatedConversation = await Message.updateMany(
-            { conversationId: req.params.conversationId, sender: { $ne: req.params.sender } },
-            { $set: { status: 'S' } }
-        );
-        res.status(200).json(updatedConversation);
-    } catch (error) {
-        console.log("error", error);
-        res.status(500).json(error);
-    }
+router.put("/seen/:conversationId/:sender", auth, async (req, res) => {
+  try {
+    const updatedConversation = await Message.updateMany(
+      {
+        conversationId: req.params.conversationId,
+        sender: { $ne: req.params.sender },
+      },
+      { $set: { status: "S" } }
+    );
+    res.status(200).json(updatedConversation);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
